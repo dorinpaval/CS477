dcf08120dcccd05a7ea5d7f92b179a615ecabd71
dorinpaval <paval.stelica@yahoo.com>
2022-07-22 08:48:15 -0500

    paval

---
+++ Assignments/w1d4/Q2image.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer();
+
+server.on('request', function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'image/jpg' });
+    let img = fs.readFileSync('./dolphin.jpg');
+    res.end(img);
+});
+server.listen(3000);
+
+const server1 = http.createServer();
+server1.on('request', (req, res) => {
+    fs.createReadStream("./dolphin2.jpg").pipe(res);
+});
+
+server1.listen(4000);
